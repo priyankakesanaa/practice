2ad6819f3b562613cffc19c45ef7d047dd7aeed4
priyankakesanaa <nagaharipriyanka@gmail.com>
2025-07-08 06:59:31 +0000

    dumy commit

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
-//
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.credentials_file)
+}
+
+resource "google_compute_instance" "default" {
+  name         = "example-vm"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  tags = ["web", "dev"]
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y nginx
+  EOT
+}
+
